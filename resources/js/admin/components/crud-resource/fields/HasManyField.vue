9527863ad543b4af1d295a761b9c8e37370351f3
Unknown <hc.ngu01@gmail.com>
2020-08-30 15:40:00 +0800

    Deving Has Many field, fetching relationships

--- resources/js/admin/components/crud-resource/fields/HasManyField.vue
+++ resources/js/admin/components/crud-resource/fields/HasManyField.vue
@@ -1,6 +1,36 @@
 <template>
 
-    
+    <article>
+
+<!--        if not searchable use dropdown-->
+
+<!--        if searchable use autocomplete-->
+        <v-autocomplete
+            v-if="searchable"
+            :value="value"
+            :items="foreignResources"
+            :loading="states.isLoading"
+            :search-input.sync="search"
+            multiple
+            outlined
+            chips
+            deletable-chips
+            clearable
+            hide-no-data
+            hide-selected
+            :item-text="itemText"
+            :item-value="itemValue"
+            placeholder="Start typing to Search"
+            prepend-icon="mdi-account_circle"
+            @input="($event) => $emit('input', $event)"
+            :rules="rules"
+            return-object
+        />
+
+        <v-select v-else/>
+
+
+    </article>
 
 
 </template>
@@ -9,13 +39,64 @@
 export default {
     name: "HasManyField",
     data() {
-        return {}
+        return {
+            states:{
+                isLoading: false,
+            },
+            search: '',
+            foreignResources: [],
+            searchTimeoutId: null,
+        }
+    },
+    watch: {
+        search(name){
+            clearTimeout(this.searchTimeoutId);
+            this.states.isLoading = true;
+            this.searchTimeoutId = setTimeout(function(){
+                this.searchResources().finally(function () {
+                    this.states.isLoading = false;
+                }.bind(this));
+            }.bind(this), 1500);
+        },
     },
     props: {
+        resourceUrl: { // assume resource is searchable is 'searchable' prop is passed in
+            type: String,
+            required: true,
+        },
         value: '',
+        searchable: {
+            type: Boolean,
+            default: true
+        },
+        itemText: {  // the foreign resource property to show in dropdown
+            type: String,
+            default: 'id',
+        },
+        itemValue: {
+            type: String,
+            default: 'id',
+        },
+        rules: {
+            type: Array,
+            default: () => [],
+        }
     },
     computed: {},
-    methods: {},
+    methods: {
+        searchResources(){
+            let uri = this.resourceUrl;
+            if(this.searchable) {
+                uri += `?search=${this.search}`;
+            }
+
+            return axios.get(uri)
+                .then((response) => {
+
+                })
+
+        },
+    },
     mounted() {
 
     },
