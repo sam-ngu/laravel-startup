ab10d9765128a6700a5c818826193c6ffaf54557
sam-ngu <sam-ngu@users.noreply.github.com>
2020-12-15 07:40:26 +0000

    Fix styling

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -6,25 +6,19 @@ use Tests\ApiTestCase;
 
 class RegisterTest extends ApiTestCase
 {
-
     public function test_user_can_register()
     {
-
     }
 
     public function test_cant_register_with_existing_username()
     {
-
     }
 
     public function test_password_must_follow_rules()
     {
-
     }
 
     public function test_confirmation_email_is_sent_once_registered()
     {
-
     }
-
 }
